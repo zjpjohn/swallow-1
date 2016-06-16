330ca032d10d2e68e4bf22b7e0f175bf8aaabcdc
qi.yin <qi.yin@dianping.com>
2016-06-16 09:50:20 +0800

    npe message size

--- swallow-common/src/main/java/com/dianping/swallow/common/internal/message/SwallowMessage.java
+++ swallow-common/src/main/java/com/dianping/swallow/common/internal/message/SwallowMessage.java
@@ -11,216 +11,220 @@ import com.dianping.swallow.common.message.Message;
 
 public class SwallowMessage implements Serializable, Message {
 
-   private static final long   serialVersionUID = -7019466307875540596L;
+    private static final long serialVersionUID = -7019466307875540596L;
 
-   private Date                generatedTime;
+    private Date generatedTime;
 
-   private Long                messageId;
+    private Long messageId;
 
-   private Map<String, String> properties;
+    private Map<String, String> properties;
 
-   private Map<String, String> internalProperties = new HashMap<String, String>();
+    private Map<String, String> internalProperties = new HashMap<String, String>();
 
-   private String              version;
+    private String version;
 
-   private String              content;
+    private String content;
 
-   private String              sha1;
+    private String sha1;
 
-   private String              type;
+    private String type;
 
-   private String              sourceIp;
+    private String sourceIp;
 
-   private Long                backupMessageId;
+    private Long backupMessageId;
 
-   @Override
-   public Date getGeneratedTime() {
-      return generatedTime;
-   }
+    @Override
+    public Date getGeneratedTime() {
+        return generatedTime;
+    }
 
-   public void setGeneratedTime(Date generatedTime) {
-      this.generatedTime = generatedTime;
-   }
+    public void setGeneratedTime(Date generatedTime) {
+        this.generatedTime = generatedTime;
+    }
 
-   @Override
-   public Long getMessageId() {
-      return messageId;
-   }
+    @Override
+    public Long getMessageId() {
+        return messageId;
+    }
 
-   public void setMessageId(Long messageId) {
-      this.messageId = messageId;
-   }
+    public void setMessageId(Long messageId) {
+        this.messageId = messageId;
+    }
 
-   /**
-    * 获取消息<em>发送方</em>使用的Swallow的版本号
-    * 
-    * @return
-    */
-   public String getVersion() {
-      return version;
-   }
+    /**
+     * 获取消息<em>发送方</em>使用的Swallow的版本号
+     *
+     * @return
+     */
+    public String getVersion() {
+        return version;
+    }
 
-   public void setVersion(String version) {
-      this.version = version;
-   }
+    public void setVersion(String version) {
+        this.version = version;
+    }
 
-   @Override
-   public Map<String, String> getProperties() {
-      return properties;
-   }
+    @Override
+    public Map<String, String> getProperties() {
+        return properties;
+    }
 
-   public void setProperties(Map<String, String> properties) {
-      this.properties = properties;
-   }
+    public void setProperties(Map<String, String> properties) {
+        this.properties = properties;
+    }
 
-   public Map<String, String> getInternalProperties() {
-      return new HashMap<String, String>(internalProperties);
-   }
-   
-   public void putInternalProperty(String key, String value){
-	   this.internalProperties.put(key, value);
-   }
-   
-   public String getInternalProperty(String key){
-	   return internalProperties.get(key);
-   }
-   
-   public void putInternalProperties(Map<String, String> properties){
-	   
-	   if(properties != null){
-		   this.internalProperties.putAll(properties);
-	   }
-   }
+    public Map<String, String> getInternalProperties() {
+        return new HashMap<String, String>(internalProperties);
+    }
 
-   @Override
-   public String getSha1() {
-      return sha1;
-   }
+    public void putInternalProperty(String key, String value) {
+        this.internalProperties.put(key, value);
+    }
 
-   public void setSha1(String sha1) {
-      this.sha1 = sha1;
-   }
+    public String getInternalProperty(String key) {
+        return internalProperties.get(key);
+    }
 
-   @Override
-   public <T> T transferContentToBean(Class<T> clazz) {
-      JsonBinder jsonBinder = JsonBinder.getNonEmptyBinder();
-      return jsonBinder.fromJson(getContent(), clazz);
-   }
+    public void putInternalProperties(Map<String, String> properties) {
 
-   public long size(){
-	   //估算，非严格
-	   long size = 0;
-	   size += contentLength();
-	   size += propertiesLength();
-	   return size;
-   }
-   
-	private long propertiesLength() {
-		if(properties == null){
-			return 0;
-		}
-		long length = 0;
-		for(Entry<String, String> property : properties.entrySet()){
-			length += property.getKey().length()*2;
-			length += property.getValue().length()*2;
-		}
-		return length;
-	}
+        if (properties != null) {
+            this.internalProperties.putAll(properties);
+        }
+    }
 
-	protected long contentLength() {
-		String content = getContent();
-		if(content == null){
-			return 0;
-		}
-		return content.length() * 2;
-	}
+    @Override
+    public String getSha1() {
+        return sha1;
+    }
 
-@Override
-   public String getContent() {
-      return content;
-   }
+    public void setSha1(String sha1) {
+        this.sha1 = sha1;
+    }
 
-   public void setContent(Object content) {
-      if (content instanceof String) {
-         this.content = (String) content;
-      } else {
-         JsonBinder jsonBinder = JsonBinder.getNonEmptyBinder();
-         this.content = jsonBinder.toJson(content);
-      }
-   }
+    @Override
+    public <T> T transferContentToBean(Class<T> clazz) {
+        JsonBinder jsonBinder = JsonBinder.getNonEmptyBinder();
+        return jsonBinder.fromJson(getContent(), clazz);
+    }
 
-   @Override
-   public String getType() {
-      return type;
-   }
+    public long size() {
+        //估算，非严格
+        long size = 0;
+        size += contentLength();
+        size += propertiesLength();
+        return size;
+    }
 
-   public void setType(String type) {
-      this.type = type;
-   }
+    private long propertiesLength() {
+        if (properties == null) {
+            return 0;
+        }
+        long length = 0;
+        for (Entry<String, String> property : properties.entrySet()) {
+            if (property.getKey() != null) {
+                length += property.getKey().length() * 2;
+            }
+            if (property.getValue() != null) {
+                length += property.getValue().length() * 2;
+            }
+        }
+        return length;
+    }
 
-   @Override
-   public String getSourceIp() {
-      return sourceIp;
-   }
+    protected long contentLength() {
+        String content = getContent();
+        if (content == null) {
+            return 0;
+        }
+        return content.length() * 2;
+    }
 
-   public void setSourceIp(String sourceIp) {
-      this.sourceIp = sourceIp;
-   }
+    @Override
+    public String getContent() {
+        return content;
+    }
 
-   public Long getBackupMessageId() {
-      return backupMessageId;
-   }
+    public void setContent(Object content) {
+        if (content instanceof String) {
+            this.content = (String) content;
+        } else {
+            JsonBinder jsonBinder = JsonBinder.getNonEmptyBinder();
+            this.content = jsonBinder.toJson(content);
+        }
+    }
 
-   public void setBackupMessageId(Long backupMessageId) {
-      this.backupMessageId = backupMessageId;
-   }
+    @Override
+    public String getType() {
+        return type;
+    }
 
-   public boolean isBackup() {
-      return backupMessageId != null;
-   }
+    public void setType(String type) {
+        this.type = type;
+    }
 
-   @Override
-   public String toString() {
-      return "SwallowMessage [ " +toSuccessKeyValuePairs()+ "&content=" + getContent() + "]";
-   }
+    @Override
+    public String getSourceIp() {
+        return sourceIp;
+    }
 
-   public String toKeyValuePairs() {
-      return toSuccessKeyValuePairs() + "&content=" + getContent();
-   }
+    public void setSourceIp(String sourceIp) {
+        this.sourceIp = sourceIp;
+    }
 
-   protected String toSuccessKeyValuePairs() {
-      return "generatedTime=" + generatedTime + "&messageId=" + messageId + "&backupMessageId=" + backupMessageId
-            + "&properties=" + properties + "&internalPropertiess=" + internalProperties + "&version=" + version
-            + "&sha1=" + sha1 + "&type=" + type + "&sourceIp=" + sourceIp;
-   }
+    public Long getBackupMessageId() {
+        return backupMessageId;
+    }
 
-   @Override
-   public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((messageId == null) ? 0 : messageId.hashCode());
-      return result;
-   }
+    public void setBackupMessageId(Long backupMessageId) {
+        this.backupMessageId = backupMessageId;
+    }
 
-   @Override
-   public boolean equals(Object obj) {
-      if (this == obj) {
-         return true;
-      }
-      if (obj == null) {
-         return false;
-      }
-      if (getClass() != obj.getClass()) {
-         return false;
-      }
-      SwallowMessage other = (SwallowMessage) obj;
-      if (messageId == null) {
-         if (other.messageId != null) {
+    public boolean isBackup() {
+        return backupMessageId != null;
+    }
+
+    @Override
+    public String toString() {
+        return "SwallowMessage [ " + toSuccessKeyValuePairs() + "&content=" + getContent() + "]";
+    }
+
+    public String toKeyValuePairs() {
+        return toSuccessKeyValuePairs() + "&content=" + getContent();
+    }
+
+    protected String toSuccessKeyValuePairs() {
+        return "generatedTime=" + generatedTime + "&messageId=" + messageId + "&backupMessageId=" + backupMessageId
+                + "&properties=" + properties + "&internalPropertiess=" + internalProperties + "&version=" + version
+                + "&sha1=" + sha1 + "&type=" + type + "&sourceIp=" + sourceIp;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((messageId == null) ? 0 : messageId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
             return false;
-         }
-      } else if (!messageId.equals(other.messageId)) {
-         return false;
-      }
-      return true;
-   }
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        SwallowMessage other = (SwallowMessage) obj;
+        if (messageId == null) {
+            if (other.messageId != null) {
+                return false;
+            }
+        } else if (!messageId.equals(other.messageId)) {
+            return false;
+        }
+        return true;
+    }
 }
